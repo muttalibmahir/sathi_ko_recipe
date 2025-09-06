1c2637dbdc711ecd6bfd5819865c026197e1cba6
muttalibmahir <muttalib.mahir@gmail.com>
2025-09-06 15:30:11 +1000

    Rename vite.ts to vite-helper.ts to avoid conflict with vite package

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { setupVite, serveStatic, log } from "./vite-helper";
 
 const app = express();
 app.use(express.json());
@@ -64,7 +64,6 @@ app.use((req, res, next) => {
   server.listen({
     port,
     host: "0.0.0.0",
-    reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
   });
